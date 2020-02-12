06a6f18780e6ea7370d5c2f63d6cca9098595384
jgromes <jgromes@users.noreply.github.com>
2020-02-12 09:32:38 +0100

    Added GPS test sketch

---
+++ software/TestSketches/GPS/GPS.ino
@@ -0,0 +1,26 @@
+#include "Debug.h"
+
+#define GPS_TX                                          PA0
+#define GPS_RX                                          PA1
+#define GPS_POWER_FET                                   PC3
+
+HardwareSerial GpsSerial(GPS_RX, GPS_TX);
+
+void setup() {
+  pinMode(GPS_POWER_FET, OUTPUT);
+  digitalWrite(GPS_POWER_FET, HIGH);
+  
+  // initialize debug port
+  FOSSASAT_DEBUG_PORT.begin(FOSSASAT_DEBUG_SPEED);
+  FOSSASAT_DEBUG_PORT.println();
+
+  // initialize UART interfaces
+  GpsSerial.begin(9600);
+}
+
+void loop() {
+  while(GpsSerial.available()) {
+    FOSSASAT_DEBUG_PORT.write(GpsSerial.read());
+  }
+
+}
