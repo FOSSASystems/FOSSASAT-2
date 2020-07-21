2d66cd37c010caa6cb274cc913a8fee9247432f4
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2020-07-21 14:08:33 +0200

    solar_determination update
    solar_determination is modified to fit into the new measure_hybridation scheme by simply averaging the two previous solar results

--- software/FossaSat2/src/ADCS/ADS/solar_determination.cpp
+++ software/FossaSat2/src/ADCS/ADS/solar_determination.cpp
@@ -11,10 +11,10 @@
 #include "../ADCS/adcs.h"
 
 /*************** Main function *****************/
-void ADS_Solar_Determination(const ADCS_CALC_TYPE luxData[ADCS_NUM_PANELS], ADCS_CALC_TYPE solarEph[ADCS_NUM_AXES],
-                             ADCS_CALC_TYPE redundantSolarEph[ADCS_NUM_AXES]) {
+void ADS_Solar_Determination(const ADCS_CALC_TYPE luxData[ADCS_NUM_PANELS], ADCS_CALC_TYPE solarEph[ADCS_NUM_AXES]) {
   // Constants definitions
   ADCS_CALC_TYPE panelUnitVector[ADCS_NUM_PANELS][ADCS_NUM_AXES] = ADCS_PANEL_UNIT_VECTOR; // Unitary vector pointing to the solar panels in inverse form
+  ADCS_CALC_TYPE redundantSolarEph[ADCS_NUM_AXES] = {0};
 
   // Maximum obtainable power/lux array
   ADCS_CALC_TYPE maxPower[ADCS_NUM_PANELS];
@@ -28,19 +28,19 @@ void ADS_Solar_Determination(const ADCS_CALC_TYPE luxData[ADCS_NUM_PANELS], ADCS
   // Main calculation
   for(uint8_t i = 0; i < ADCS_NUM_AXES; i++) {
     solarEph[i] = 0;
-    redundantSolarEph[i] = 0;
     for(uint8_t j = 0; j < ADCS_NUM_AXES; j++) {
       solarEph[i] += panelUnitVector[i][j]*luxData[j]*(1.0/maxPower[j]);
       redundantSolarEph[i] += panelUnitVector[ADCS_NUM_AXES + i][j]*luxData[ADCS_NUM_AXES + j]*(1.0/maxPower[ADCS_NUM_AXES + j]);
     }
   }
 
-  // Normalize the solar ephemerides
+  // Normalize the solar ephemerides and mix them both (simple averaging)
   ADCS_CALC_TYPE solarEphNorm = ADCS_Add_Tolerance(ADCS_VectorNorm(solarEph),0);
   ADCS_CALC_TYPE redundantSolarEphNorm = ADCS_Add_Tolerance(ADCS_VectorNorm(redundantSolarEph),0);
 
   for(uint8_t i = 0; i < ADCS_NUM_AXES; i++) {
-    solarEph[i] *= (1.0/solarEphNorm);
-    redundantSolarEph[i] *= (1.0/redundantSolarEphNorm);
+    redundantSolarEph[i] *= (0.5)*(1.0/redundantSolarEphNorm);
+    solarEph[i] *= (0.5)*1.0/solarEphNorm);
+    solarEph[i] += redundantSolarEph[i];
   }
 }
