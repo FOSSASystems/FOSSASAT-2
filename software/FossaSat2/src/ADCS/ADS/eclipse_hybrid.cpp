6b38480a73b6a863f4ebc92947104a6e4c8a319e
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2020-07-27 04:00:28 +0200

    Eclipse_hybrid Update
    The rotation matrix is now obtained by means of the Rodrigues Formula.

--- software/FossaSat2/src/ADCS/ADS/eclipse_hybrid.cpp
+++ software/FossaSat2/src/ADCS/ADS/eclipse_hybrid.cpp
@@ -32,10 +32,42 @@ void ADS_Eclipse_Hybrid(const ADCS_CALC_TYPE magData[ADCS_NUM_AXES], const ADCS_
   lvFrame[1] = epheGain * magEphe[1];
   lvFrame[2] = epheGain * magEphe[2];
 
-  // Euler angles matrix -dot product between the two unitary vectors-
-  for(uint8_t i = 0; i < ADCS_NUM_AXES; i++) {
-    for(uint8_t j = 0; j < ADCS_NUM_AXES; j++) {
-      rotationMatrix[i][j] = lvFrame[j] * bodyFrame[i];
+  // Dot product between the two (cos(alpha) == a dot b)
+  ADCS_CALC_TYPE alphaDot = bodyFrame[0]*lvFrame[0]+bodyFrame[1]*lvFrame[1]+bodyFrame[2]*lvFrame[2];
+
+  // Cross product between the two (sin(alpha) == norm(a cross b))
+  ADCS_CALC_TYPE rotationAxe[ADCS_NUM_AXES];
+  rotationAxe[0] = bodyFrame[2]*lvFrame[1]-bodyFrame[1]*lvFrame[2];
+  rotationAxe[1] = bodyFrame[0]*lvFrame[2]-bodyFrame[2]*lvFrame[0];
+  rotationAxe[2] = bodyFrame[1]*lvFrame[0]-bodyFrame[0]*lvFrame[1];
+
+  ADCS_CALC_TYPE alphaCross = sqrt(pow(rotationAxe[0],2)+pow(rotationAxe[1],2)+pow(rotationAxe[2],2));
+
+  // Euler angles matrix -Rodrigues' formula-
+  ADCS_CALC_TYPE rotationMatrixAux1[ADCS_NUM_AXES][ADCS_NUM_AXES] = {{alphaDot, 0, 0},
+                                                                     {0, alphaDot, 0},
+                                                                     {0, 0, alphaDot}};
+  ADCS_CALC_TYPE rotationMatrixAux2[ADCS_NUM_AXES][ADCS_NUM_AXES];
+  for(uint32_t i = 0; i < ADCS_NUM_AXES; i++){
+    for(uint32_t j = 0; j < ADCS_NUM_AXES; j++){
+        rotationMatrixAux2[i][j] = (1.0-alphaDot)*(rotationAxe[i]*rotationAxe[j]);
     }
   }
+
+  ADCS_CALC_TYPE rotationMatrixAux3[ADCS_NUM_AXES][ADCS_NUM_AXES] = {{0, -1.0*rotationAxe[2], rotationAxe[1]},
+                                                                     {rotationAxe[2], 0, -1.0*rotationAxe[0]},
+                                                                     {-1.0*rotationAxe[1], rotationAxe[0], 0}};
+  for(uint32_t i = 0; i < ADCS_NUM_AXES; i++){
+    for(uint32_t j = 0; j < ADCS_NUM_AXES; j++){
+        rotationMatrixAux3[i][j] *= -1.0*alphaCross;
+    }
+  }
+
+  // Final computation
+  for(uint32_t i = 0; i < ADCS_NUM_AXES; i++){
+    for(uint32_t j = 0; j < ADCS_NUM_AXES; j++){
+        rotationMatrix[i][j] = rotationMatrixAux1[i][j]+rotationMatrixAux2[i][j]+rotationMatrixAux3[i][j];
+    }
+  }
+
 }
